9a586ada9b8dc2c621556c9ea51f766adb156702
Amrit Singh <amrits.deo@gmail.com>
2024-07-18 07:47:57 +0530

    Updated Base

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import EmblaCarousel from "./EmblaCarousel/EmblaCarousel";
 import Navbar from "./Navbar";
 import Bar from "./Bar";
@@ -6,12 +5,3 @@ import BasicCarousel from "./BasicCarousel/BasicCarousel";
 import MenuList from "./MenuList";
 
 export { EmblaCarousel, Navbar, Bar, BasicCarousel, MenuList };
-=======
-import EmblaCarousel from "./EmblaCarousel/EmblaCarousel";
-import Navbar from "./Navbar";
-import Bar from "./Bar";
-import BasicCarousel from "./BasicCarousel/BasicCarousel";
-import MenuList from "./MenuList";
-
-export { EmblaCarousel, Navbar, Bar, BasicCarousel, MenuList };
->>>>>>> cfd55ba0bcef38e9e30dedd423ec87af8498d47f
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 import Home from "./Home";
 
 export { Home };
-=======
-import Home from "./Home";
-
-export { Home };
->>>>>>> cfd55ba0bcef38e9e30dedd423ec87af8498d47f
